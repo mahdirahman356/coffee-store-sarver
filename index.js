deb0c78d06b4a691b5241b1396b568e7bedc3de8
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2024-04-23 12:58:41 +0600

    added a user data and setup post delete and get

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, Collection } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -25,12 +25,12 @@ async function run() {
     await client.connect();
     const database = client.db("coffeestDB");
     const coffeesCollection = database.collection("coffees");
+    const userCollection = database.collection("users")
 
     app.get("/coffees", async(req, res) => {
       const cursor = coffeesCollection.find();
       const  result = await cursor.toArray()
       res.send(result)
-      console.log(result)
     })
     
     app.get("/coffees/:id", async(req, res) => {
@@ -73,6 +73,30 @@ async function run() {
         const query = { _id: new ObjectId(id) };
         const result = await coffeesCollection.deleteOne(query);
         res.send(result)
+    })
+
+    // User Collection
+
+    app.get("/users", async(req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray()
+      res.send(result)
+
+    })
+
+    app.post("/users", async(req, res) => {
+      const user = req.body
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+
+    })
+
+    app.delete("/users/:id", async(req, res) => {
+          const id = req.params.id
+          const query = { _id: new ObjectId(id) };
+          const result = await userCollection.deleteOne(query);
+          res.send(result)
+
     })
 
     // Send a ping to confirm a successful connection
